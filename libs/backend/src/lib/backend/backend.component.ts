48b7c7b5510c2c23731ce2645126deaa5da9a61b
unikorm <adaled00@gmail.com>
2023-12-07 13:36:44 +0100

    libs created, frontend and backemd in root folder libs

---
+++ libs/backend/src/lib/backend/backend.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hrsd-kniznica-backend',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './backend.component.html',
+  styleUrl: './backend.component.css',
+})
+export class BackendComponent {}
---
+++ libs/frontend/src/lib/frontend/frontend.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hrsd-kniznica-frontend',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './frontend.component.html',
+  styleUrl: './frontend.component.css',
+})
+export class FrontendComponent {}
