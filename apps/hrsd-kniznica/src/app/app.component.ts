b18e1324839d9fe0087e9d4b94d2bf20273d1507
unikorm <adaled00@gmail.com>
2023-12-08 00:09:49 +0100

    static UI in process

--- apps/hrsd-kniznica/src/app/app.component.ts
+++ apps/hrsd-kniznica/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 @Component({
   standalone: true,
-  imports: [RouterModule, MainComponent],
+  imports: [RouterModule, MainComponent, AboutComponent],
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   templateUrl: './app.component.html',
--- apps/hrsd-kniznica/src/app/main/main.component.ts
+++ apps/hrsd-kniznica/src/app/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookComponent } from '../book/book.component';
+import { FormToAddBookComponent } from '../formToAddBook/formToAddBook.component';
 
 @Component({
   selector: 'hrsd-kniznica-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BookComponent, FormToAddBookComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
