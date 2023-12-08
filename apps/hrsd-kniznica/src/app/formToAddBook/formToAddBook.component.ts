f2f3356f4258d40b3a98402902f2bb853b2daa87
unikorm <adaled00@gmail.com>
2023-12-08 01:34:17 +0100

    some content on section to add book, i create reactive form with help of good understandable documentation and Bart cause he has actual information and that is crucial for me now, GPT know Angular 12 and Bart 17 soo.. that is only option and i create there solid structure on backend cominication too, i check it in documentation and it's good enough so I know now already how to do functional form ready to send on back...

--- apps/hrsd-kniznica/src/app/formToAddBook/formToAddBook.component.ts
+++ apps/hrsd-kniznica/src/app/formToAddBook/formToAddBook.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'hrsd-kniznica-form-to-add-book',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './formToAddBook.component.html',
   styleUrl: './formToAddBook.component.scss',
 })
-export class FormToAddBookComponent {}
+export class FormToAddBookComponent implements OnInit {
+
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(),
+      author: new FormControl(),
+      description: new FormControl()
+    }) 
+  }
+
+}
