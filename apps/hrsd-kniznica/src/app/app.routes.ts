58b13e09fb832b4d7aab19f939e3cca77391ca5f
unikorm <adaled00@gmail.com>
2023-12-08 02:16:12 +0100

    i know i want make now static look, but again i try make routing between main and about and nothing, from start of learning AG is routing a bit problem for me, it's similar to react but idk how it works here, didn't get it for now...slp

--- apps/hrsd-kniznica/src/app/app.routes.ts
+++ apps/hrsd-kniznica/src/app/app.routes.ts
@@ -1,3 +1,18 @@
+
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: MainComponent,
+        title: 'Library',
+    },
+    {
+        path: '/blabla',  // test, here will be dynamic route
+        component: AboutComponent,
+        title: '',
+    }
+];
--- apps/hrsd-kniznica/src/app/book/book.component.ts
+++ apps/hrsd-kniznica/src/app/book/book.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { RouterModule } from '@nestjs/core';
+// import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'hrsd-kniznica-book',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule],  // routing is problem a bit for me
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
