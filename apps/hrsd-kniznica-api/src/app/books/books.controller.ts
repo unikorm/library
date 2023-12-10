2ed012e7f5faa2aea46901ba980d6e78e2200554
unikorm <adaled00@gmail.com>
2023-12-10 23:51:51 +0100

    get, post and put is working but delete has some problem so i try something here

--- apps/hrsd-kniznica-api/src/app/books/books.controller.ts
+++ apps/hrsd-kniznica-api/src/app/books/books.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get, Post, Delete, Put,  Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete,  Body, Param } from '@nestjs/common';
 
 import { AddBookDTO } from './DTO/add-book.dto';
 import { BooksService } from './books.service';
@@ -11,28 +11,28 @@ export class BooksController {
     constructor(private readonly bookService: BooksService) {}
 
     @Get()  // this will be on getting all books on list of books
-    async allBooks(): Promise<Book[]> {
+    allBooks(): Promise<Book[]> {
         return this.bookService.findAll();
     }
 
     @Get(':name')  // this will be on getting separate book when user click on about 
-    async bookByName(@Param('name') name): Promise<Book> {
+    bookByName(@Param('name') name): Promise<Book> {
         return this.bookService.findOne(name)
     }
 
-    @Post()
-    addBook(@Body() addBookDTO: AddBookDTO): string {
-        return `Name ${addBookDTO.name}, and author ${addBookDTO.author}`;
+    @Post()  // this will create new book from form to add new one
+    addBook(@Body() addBookDTO: AddBookDTO): Promise<Book> {
+        return this.bookService.create(addBookDTO)
     }
 
     @Delete(':id')
-    removeBook(@Param('id') id): string {
-        return `remove book ${id}`;
+    removeBook(@Param('id') id): Promise<Book> {
+        return this.bookService.remove(id);
     }
 
-    @Put(':id')
-    updateStatus(@Body() addBookDTO: AddBookDTO, @Param('id') id): string {
-        return `update ${id} book ${addBookDTO.name}`;
+    @Put(':id') // this update status of loan/return book (but it works only with id, not name)
+    updateStatus(@Body() addBookDTO: AddBookDTO, @Param('id') id): Promise<Book> {
+        return this.bookService.updateStatus(id, addBookDTO);
     }
 
 }
--- apps/hrsd-kniznica-api/src/app/books/books.service.ts
+++ apps/hrsd-kniznica-api/src/app/books/books.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
-
 import { Book } from './interfaces/book.interface';
 
 
@@ -18,5 +17,18 @@ export class BooksService {
 
     async findOne(name: string): Promise<Book> {
         return await this.bookModel.findOne({ name: name });
+    }
+
+    async create(book: Book): Promise<Book> {
+        const newBook = new this.bookModel(book);
+        return await newBook.save();
+    }
+
+    async remove(id: string): Promise<Book> {
+        return await this.bookModel.findByIdAndRemove({ _id: id}).exec();
+    }
+
+    async updateStatus(id: string, book: Book): Promise<Book> {
+        return await this.bookModel.findByIdAndUpdate(id, book, { new: true })
     }
 }
